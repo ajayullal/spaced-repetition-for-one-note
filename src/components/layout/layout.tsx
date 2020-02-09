28e0ddccbad7a0b05e316faf7220f7a14753916c
Ajay Ullal <akusumav@akamai.com>
2020-02-09 16:12:59 +0530

    Delete breadcrumbs

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,7 +3,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles, Theme, createStyles, Typography, Link } from '@material-ui/core';
 import Header from '../header/Header';
 import ILayout from './ILayout';
-import { Breadcrumbs } from '@material-ui/core';
 import routeService from '../.../../../services/route.service';
 import {withRouter} from 'react-router-dom';
 import ErrorAlert from '../error-alert/ErrorAlert';
@@ -34,29 +33,6 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default withRouter(function Layout({ errorMessage, routeInfo, children, hideNavDrawer = false, history }: any) {
   const classes = useStyles();
-  let breadCrumbs = useRef([]);
-
-  useEffect(() => {
-    breadCrumbs.current = routeService.getBreadCrumbs(routeInfo.name);
-  }, [routeInfo.name]);
-
-  const breadCrumbsElem = (
-    <Breadcrumbs className={classes.breadcrumb} aria-label="breadcrumb">
-      { 
-        breadCrumbs.current?.map((breadCrumb: any) => {
-          return (
-            <Link key={breadCrumb.name} color="inherit" href="/" className={classes.link}>
-              {breadCrumb.name}
-            </Link>
-          );
-        })
-      }
-      
-      <Typography color="textPrimary" className={classes.link}>
-        {routeInfo.name}
-      </Typography>
-    </Breadcrumbs>
-  );
 
   return (
     <div className={classes.root}>
@@ -68,7 +44,6 @@ export default withRouter(function Layout({ errorMessage, routeInfo, children, h
 
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        {breadCrumbs.current?.length > 0? breadCrumbsElem : null}
         {children}
       </main>
     </div>
