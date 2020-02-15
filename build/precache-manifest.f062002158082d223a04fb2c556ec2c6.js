4c267da0f83be04894548aaaab509196776998fe
Ajay Ullal <akusumav@akamai.com>
2020-02-15 14:24:39 +0530

    App build

---
+++ build/precache-manifest.f062002158082d223a04fb2c556ec2c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5465950dd1b3659380d366278475c3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16e6d0761378158b20d2",
+    "url": "/static/css/main.7d429b6f.chunk.css"
+  },
+  {
+    "revision": "612ab0c5749f8b954c45",
+    "url": "/static/js/2.64597f1d.chunk.js"
+  },
+  {
+    "revision": "374a015f9bd7dbb4f757a46f1c6a7946",
+    "url": "/static/js/2.64597f1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16e6d0761378158b20d2",
+    "url": "/static/js/main.45c83b51.chunk.js"
+  },
+  {
+    "revision": "92c5276df72cff4486d3",
+    "url": "/static/js/runtime-main.e931e2ff.js"
+  }
+]);
