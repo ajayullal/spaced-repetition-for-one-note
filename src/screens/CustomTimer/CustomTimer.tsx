85d788d1e711e5ee870ea4e5eebde44232be4e20
Ajay Ullal <ajayullal91@gmail.com>
2020-07-03 03:05:52 +0530

    CP

--- src/screens/CustomTimer/CustomTimer.tsx
+++ src/screens/CustomTimer/CustomTimer.tsx
@@ -9,6 +9,7 @@ export default () => {
         <>
             <Layout hideNavDrawer={true} routeInfo={routerService.getRouteInfo('customTimer')}>
                 <>
+                    <h1>Hello World</h1>
                     <TaskNameInput></TaskNameInput>
                 </>
             </Layout>
